e8993d62d48c96a9494716857076e5a1d47f38c9
zachakbar <zach@thinkbigfish.com>
2017-02-28 13:58:52 -0700

    added back go home button

--- 404.php
+++ 404.php
@@ -4,7 +4,8 @@
 
 		<section class="content" role="main">
 			
-		<h2>Oops, page not found :(</h2>
+			<h2>Oops, page not found :(</h2>
+			<a href="<?php bloginfo('url'); ?>" class="btn">Back to homepage</a>
 		
 		</section>
 	
