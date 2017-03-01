f0afec5eac15753771b9d48c0f1ffc5a2617c44a
zachakbar <zach@thinkbigfish.com>
2017-02-28 17:40:20 -0700

    default home page template

---
+++ front-page.php
@@ -0,0 +1,22 @@
+<?php get_header(); ?>
+
+<?php if ( have_posts() ): ?>
+	<?php while ( have_posts() ) : the_post(); ?>
+
+	<section role="main">
+		<div class="wrap">
+			<article>
+				<header>
+					<h1><?php the_title(); ?></h1>
+				</header>
+					
+				<?php the_content(); ?>
+				
+			</article>
+		</div>
+	</section>
+
+	<?php endwhile; ?>
+<?php endif; ?>
+
+<?php get_footer(); ?>
