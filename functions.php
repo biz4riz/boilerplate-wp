4311dada75e544042b0f478e318394a1c1ce9a75
zachakbar <zach@thinkbigfish.com>
2017-06-15 12:01:54 -0700

    added script/style preload/hide emojis

--- functions.php
+++ functions.php
@@ -104,6 +104,47 @@ function remove_gen_version() {
 	return '';
 }
 
+// Add preload tag to web font scripts and styles
+function add_script_attributes($tag, $handle) {
+	if ( 'theme_typekit' !== $handle && 'fontawesome' !== $handle ){
+		return $tag;
+	}
+	return str_replace( ' src', ' preload src', $tag );
+}
+
+function add_style_attributes($html, $handle) {
+	if ( 'layout' !== $handle && 'styles' !== $handle ){
+		return $html;
+	}
+	return str_replace( ' href', ' preload href', $html );
+}
+
+/************************************************************************/
+/* REMOVE EMOJI STUFF
+/************************************************************************/
+
+function disable_emojicons_tinymce( $plugins ) {
+	if ( is_array( $plugins ) ) {
+		return array_diff( $plugins, array( 'wpemoji' ) );
+	} else {
+		return array();
+	}
+}
+function disable_wp_emojicons() {
+	// all actions related to emojis
+	remove_action( 'admin_print_styles', 'print_emoji_styles' );
+	remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+	remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+	remove_action( 'wp_print_styles', 'print_emoji_styles' );
+	remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
+	remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
+	remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );
+	// filter to remove TinyMCE emojis
+	add_filter( 'tiny_mce_plugins', 'disable_emojicons_tinymce' );
+}
+add_action( 'init', 'disable_wp_emojicons' );
+add_filter( 'emoji_svg_url', '__return_false' );
+
 /************************************************************************/
 /* ACTIONS & FILTERS
 /************************************************************************/
@@ -114,5 +155,7 @@ add_action( 'wp_enqueue_scripts', 'theme_typekit' );
 add_action( 'wp_head', 'theme_typekit_const' );
 add_filter( 'tiny_mce_before_init', 'color_options' );
 add_filter( 'style_loader_src', 'remove_cssjs_ver', 10, 2 );
+//add_filter( 'script_loader_tag', 'add_script_attributes', 10, 2 );
+add_filter( 'style_loader_tag', 'add_style_attributes', 10, 2 );
 add_filter( 'the_generator', 'remove_gen_version' );
 
